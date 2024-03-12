20a05e7bb787900322945a9313dd3617fe9a8428
Vikram sharma <sharma.277@wright.edu>
2024-03-12 17:38:39 -0400

    Backend and repo setup

--- backend/Models/messageModel.js
+++ backend/Models/messageModel.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose')
+import mongoose from "mongoose";
 
 const messageModel = mongoose.Schema({
     sender :{type:mongoose.Schema.Types.ObjectId,ref:"User"},
--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,31 @@
-const express = require("express");
-const dotenv = require("dotenv");
-const { chats } = require("./data/data");
-const cors = require('cors');
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import morgan from 'morgan';
+import http from 'http'
+
+import {chats} from './data/data.js'
 
 const app = express()
 dotenv.config();
 
 app.use(cors());
-
-const morgan = require("morgan");
 app.use(morgan("dev"));
 
-app.get('/', (req,res)=>{
-    res.send("API is running successfully");
-});
-app.get('/api/chat', (req,res)=>{
-    res.json({"chats":chats});
-});
-app.get('/api/chat/:id', (req,res)=>{
-    // console.log(req);
-    const singleChat = chats.find((c)=>c._id === req.params.id)
-    res.send(singleChat);
-});
+// app.get('/', (req,res)=>{
+//     res.send("API is running successfully");
+// });
+// app.get('/api/chat', (req,res)=>{
+//     res.json({"chats":chats});
+// });
+// app.get('/api/chat/:id', (req,res)=>{
+//     // console.log(req);
+//     const singleChat = chats.find((c)=>c._id === req.params.id)
+//     res.send(singleChat);
+// });
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () =>{ 
+    console.log(`Server started on port ${PORT}`)
+});
